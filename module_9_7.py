4f566d63c2893b8ebc681c12d4893d472a1ee126
Swagger <vladimiryaroslavcev3@gmail.com>
2024-12-10 00:45:35 +0300

    Add files via upload

---
+++ module_9_7.py
@@ -0,0 +1,24 @@
+def is_prime(func):
+    def wrapper(*args, **kwargs):
+
+        result = func(*args, **kwargs)
+
+        if result < 2:
+            print("Составное")
+        elif all(result % i != 0 for i in range(2, int(result ** 0.5) + 1)):
+            print("Простое")
+        else:
+            print("Составное")
+
+        return result
+
+    return wrapper
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+
+
+result = sum_three(2, 3, 6)  
+print(result)
